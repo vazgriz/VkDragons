9d5bd294c6c0d99b9a4267b1cd79aef327a72dac
vazgriz <vazgriz@gmail.com>
2017-08-26 20:17:34 -0500

    Add Input to Scene

--- VkDragons/Scene.cs
+++ VkDragons/Scene.cs
@@ -6,10 +6,12 @@ namespace VkDragons {
     public class Scene : IDisposable {
         Renderer renderer;
         Camera camera;
+        Input input;
 
         public Scene(Window window) {
             renderer = new Renderer(window);
             camera = new Camera(45, window.FramebufferWidth, window.FramebufferHeight);
+            input = new Input(window, this, renderer, camera);
         }
 
         public void Dispose() {
