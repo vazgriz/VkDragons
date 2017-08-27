d06ec0c8caef73eebb758c64a62a878b889e0465
vazgriz <vazgriz@gmail.com>
2017-08-26 20:50:36 -0500

    Add Vertex descriptions

--- VkDragons/Model.cs
+++ VkDragons/Model.cs
@@ -125,5 +125,69 @@ namespace VkDragons {
             texCoordsStagingBuffer = Copy(commandBuffer, texCoordsBuffer, mesh.TexCoords);
             indexStagingBuffer = Copy(commandBuffer, indexBuffer, mesh.Indices);
         }
+
+        public static List<VkVertexInputBindingDescription> BindingDescriptions {
+            get {
+                return new List<VkVertexInputBindingDescription> {
+                    new VkVertexInputBindingDescription {    //position
+                        binding = 0,
+                        stride = (uint)Interop.SizeOf<Vector3>(),
+                        inputRate = VkVertexInputRate.Vertex
+                    },
+                    new VkVertexInputBindingDescription {    //normal
+                        binding = 1,
+                        stride = (uint)Interop.SizeOf<Vector3>(),
+                        inputRate = VkVertexInputRate.Vertex
+                    },
+                    new VkVertexInputBindingDescription {    //tangent
+                        binding = 2,
+                        stride = (uint)Interop.SizeOf<Vector3>(),
+                        inputRate = VkVertexInputRate.Vertex
+                    },
+                    new VkVertexInputBindingDescription {    //binormal
+                        binding = 3,
+                        stride = (uint)Interop.SizeOf<Vector3>(),
+                        inputRate = VkVertexInputRate.Vertex
+                    },
+                    new VkVertexInputBindingDescription {    //texcoord
+                        binding = 4,
+                        stride = (uint)Interop.SizeOf<Vector2>(),
+                        inputRate = VkVertexInputRate.Vertex
+                    },
+                };
+            }
+        }
+
+        public static List<VkVertexInputAttributeDescription> AttributeDescriptions {
+            get {
+                return new List<VkVertexInputAttributeDescription> {
+                    new VkVertexInputAttributeDescription {
+                        binding = 0,
+                        location = 0,
+                        format = VkFormat.R32g32b32Sfloat,
+                    },
+                    new VkVertexInputAttributeDescription {
+                        binding = 1,
+                        location = 1,
+                        format = VkFormat.R32g32b32Sfloat,
+                    },
+                    new VkVertexInputAttributeDescription {
+                        binding = 2,
+                        location = 2,
+                        format = VkFormat.R32g32b32Sfloat,
+                    },
+                    new VkVertexInputAttributeDescription {
+                        binding = 3,
+                        location = 3,
+                        format = VkFormat.R32g32b32Sfloat,
+                    },
+                    new VkVertexInputAttributeDescription {
+                        binding = 4,
+                        location = 4,
+                        format = VkFormat.R32g32Sfloat,
+                    },
+                };
+            }
+        }
     }
 }
