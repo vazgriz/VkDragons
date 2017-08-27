952523feed24125e276bcf68295ef379af7c98de
vazgriz <vazgriz@gmail.com>
2017-08-26 19:44:52 -0500

    Add Camera to scene

--- VkDragons/Scene.cs
+++ VkDragons/Scene.cs
@@ -5,9 +5,11 @@ using CSGL.GLFW;
 namespace VkDragons {
     public class Scene : IDisposable {
         Renderer renderer;
+        Camera camera;
 
         public Scene(Window window) {
             renderer = new Renderer(window);
+            camera = new Camera(45, window.FramebufferWidth, window.FramebufferHeight);
         }
 
         public void Dispose() {
